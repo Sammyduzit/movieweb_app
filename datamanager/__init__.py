6356e49e5eb57068066299628b3215fcf8d53356
Sammyduzit <sammy.duzit999@gmail.com>
2025-05-30 13:34:59 +0200

    clean up module structure

--- datamanager/__init__.py
+++ datamanager/__init__.py
@@ -1,9 +1,7 @@
 from .database import db, init_database
-from .data_models import User, Movie, Review
+from .data_models import User, Movie, Review, TriviaScore
 from .data_manager_interface import DataManagerInterface
 from .sqlite_data_manager import SQLiteDataManager
-from .omdb_service import OMDbService
-from .rapidapi_service import RapidAPIService
 
-__all__ = ['db', 'init_database', 'User', 'Movie', 'Review', 'DataManagerInterface',
-           'SQLiteDataManager', 'OMDbService', 'RapidAPIService']
+__all__ = ['db', 'init_database', 'User', 'Movie', 'Review', 'TriviaScore',
+           'DataManagerInterface', 'SQLiteDataManager']
