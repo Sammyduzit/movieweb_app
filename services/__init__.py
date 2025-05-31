0e1f3c32622aed22fe2fe0199bd0405f5c3964ea
Sammyduzit <sammy.duzit999@gmail.com>
2025-05-31 20:17:35 +0200

    Register OpenAI API Key configuration

--- services/__init__.py
+++ services/__init__.py
@@ -4,7 +4,8 @@ from .review_service import ReviewService
 from .omdb_service import OMDbService
 from .api_usage_tracker import APIUsageTracker
 from .rapidapi_service import RapidAPIService
+from .openai_service import OpenAIService
 from .trivia_service import TriviaService
 
 __all__ = ['UserService', 'MovieService', 'ReviewService', 'TriviaService',
-           'OMDbService', 'RapidAPIService', 'APIUsageTracker']
+           'OMDbService', 'RapidAPIService', 'OpenAIService', 'APIUsageTracker']
