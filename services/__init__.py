ec56860ef66bcb021f4b607bc41350235968d06c
Sammyduzit <sammy.duzit999@gmail.com>
2025-05-31 17:26:41 +0200

    update for API usage tracking

--- services/__init__.py
+++ services/__init__.py
@@ -1,9 +1,10 @@
 from .user_service import UserService
 from .movie_service import MovieService
 from .review_service import ReviewService
-from .trivia_service import TriviaService
 from .omdb_service import OMDbService
+from .api_usage_tracker import APIUsageTracker
 from .rapidapi_service import RapidAPIService
+from .trivia_service import TriviaService
 
 __all__ = ['UserService', 'MovieService', 'ReviewService', 'TriviaService',
-           'OMDbService', 'RapidAPIService']
+           'OMDbService', 'RapidAPIService', 'APIUsageTracker']
