f5bbdb3e526987ee229fe78c35fbdf5dd32b1cbc
Sammyduzit <sammy.duzit999@gmail.com>
2025-06-02 08:43:59 +0200

    fix and update

--- services/__init__.py
+++ services/__init__.py
@@ -1,11 +1,19 @@
-from .user_service import UserService
-from .movie_service import MovieService
-from .review_service import ReviewService
-from .omdb_service import OMDbService
 from .api_usage_tracker import APIUsageTracker
-from .rapidapi_service import RapidAPIService
+from .movie_service import MovieService
+from .omdb_service import OMDbService
 from .openai_service import OpenAIService
+from .rapidapi_service import RapidAPIService
+from .review_service import ReviewService
 from .trivia_service import TriviaService
+from .user_service import UserService
 
-__all__ = ['UserService', 'MovieService', 'ReviewService', 'TriviaService',
-           'OMDbService', 'RapidAPIService', 'OpenAIService', 'APIUsageTracker']
+__all__ = [
+    'APIUsageTracker',
+    'MovieService',
+    'OMDbService',
+    'OpenAIService',
+    'RapidAPIService',
+    'ReviewService',
+    'TriviaService',
+    'UserService'
+]
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,11 +1,21 @@
+from .app_helpers import (
+    get_error_template_context, print_startup_info, register_error_handlers
+)
+from .decorators import (
+    handle_validation_errors, require_movie_exists, require_user,
+    require_user_and_movie
+)
 from .template_helpers import (
-    format_percentage, get_performance_badge, get_rank_display,
-    format_trivia_type, format_date, truncate_text, get_difficulty_style,
-    format_rating, get_poster_url, pluralize, register_template_helpers
+    format_date, format_percentage, format_rating, format_trivia_type,
+    get_difficulty_style, get_performance_badge, get_poster_url,
+    get_rank_display, pluralize, register_template_helpers, truncate_text
 )
 
 __all__ = [
-    'format_percentage', 'get_performance_badge', 'get_rank_display',
-    'format_trivia_type', 'format_date', 'truncate_text', 'get_difficulty_style',
-    'format_rating', 'get_poster_url', 'pluralize', 'register_template_helpers'
+    'get_error_template_context', 'print_startup_info', 'register_error_handlers',
+    'handle_validation_errors', 'require_movie_exists', 'require_user',
+    'require_user_and_movie',
+    'format_date', 'format_percentage', 'format_rating', 'format_trivia_type',
+    'get_difficulty_style', 'get_performance_badge', 'get_poster_url',
+    'get_rank_display', 'pluralize', 'register_template_helpers', 'truncate_text'
 ]
